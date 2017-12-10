de7d56d6d065f9fc906fea48b9764c88791c69f5
Jayant Tang <578900679@qq.com>
2017-12-10 16:19:23 +0800

    Add files via upload

---
+++ usart1.h
@@ -0,0 +1,41 @@
+#ifndef __USART1_H
+#define __USART1_H
+
+#define USART_DEBUG
+#ifdef USART_DEBUG
+#define PRINT(info) {printf(info);}
+#else
+#define PRINT(info) {}
+#endif
+
+#include "stm32f4xx.h"
+
+#include <stdio.h>
+
+#define RX_BUFFER_LEN 256
+#define TX_BUFFER_LEN 256
+
+extern u8 RX_BUFFER[RX_BUFFER_LEN];//接受缓冲区
+extern u8 TX_BUFFER[TX_BUFFER_LEN];//发送缓冲区
+
+extern u8 RX_Availiable; //当缓冲区内有数据时，此变量为1
+
+void usart1_Init(u32);
+//串口1初始化
+
+void usart1_SendByte(u8);
+//发送一个字节
+
+void usart1_SendStr(char*);
+//发送一个字符串，以\0结尾
+
+void usart1_SendData(u8*,u8);
+//发送一个一定长度的数据包，不超过256字节。
+
+u8 usart1_ReadByte(void);
+//读取缓冲区中的一个字节，在RX_Acceptable为1时再调用此函数
+
+
+#endif
+
+
